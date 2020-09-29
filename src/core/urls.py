69a2d62c9c666995d484f45c3e067076e4bd1ba7
MichaelNest <nesterenkom385@gmail.com>
2020-09-29 18:09:51 +0300

    Create and set app 'pages'.

--- src/core/urls.py
+++ src/core/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    # path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', include('pages.urls', namespace='pages')),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
     path('users/', include('django.contrib.auth.urls')),
