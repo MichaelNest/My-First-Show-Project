6c78b749ae30713f6c685c897fa7682f140a7d44
MichaelNest <nesterenkom385@gmail.com>
2020-09-30 17:28:23 +0300

    Create files registration/password_reset_....html

--- src/core/settings.py
+++ src/core/settings.py
@@ -132,3 +132,5 @@ LOGIN_REDIRECT_URL = 'pages:home'
 LOGOUT_REDIRECT_URL = 'pages:home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' #for testing post server in terminal
