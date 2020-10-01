f9d8ef4e17431bddc1c45ba6723601c80d728285
MichaelNest <nesterenkom385@gmail.com>
2020-10-01 17:33:20 +0300

    Create and set files: articles/urls.py, articles/views/class ArticleListView, articles/templates/articles/article_list.html

--- src/core/urls.py
+++ src/core/urls.py
@@ -20,6 +20,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     # path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('', include('pages.urls', namespace='pages')),
+    path('articles/', include('articles.urls', namespace='articles')),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
     path('users/', include('django.contrib.auth.urls')),
