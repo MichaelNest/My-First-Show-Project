4b400e4cd2a1a1683a90386792a3bf7b885d56ce
MichaelNest <nesterenkom385@gmail.com>
2020-10-02 16:42:21 +0300

    Set article/views.py class ArticleCreateView() - add def form_valid and remove 'author' - by create new article - author will add automaticly.

--- src/articles/views.py
+++ src/articles/views.py
@@ -25,5 +25,9 @@ class ArticleDeleteView(DeleteView):
 
 class ArticleCreateView(CreateView):
     model = Article
-    fields = ['title', 'body', 'author']
+    fields = ['title', 'body']
     template_name = 'articles/article_new.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
