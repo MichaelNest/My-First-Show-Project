d6ec9f19c5dcf3349c114fcabb4abc48412e249e
MichaelNest <nesterenkom385@gmail.com>
2020-10-02 17:04:33 +0300

    Add LoginRequiredMixin and login_url to other classes articles/views.py

--- src/articles/views.py
+++ src/articles/views.py
@@ -1,32 +1,36 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import DeleteView, UpdateView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Article
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = 'articles/article_list.html'
+    login_url = 'login'
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = 'articles/article_detail.html'
+    login_url = 'login'
 
-
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
     model = Article
     fields = ['title', 'body']
     template_name = 'articles/article_edit.html'
+    login_url = 'login'
 
-
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
     model = Article
     template_name = 'articles/article_delete.html'
     success_url = reverse_lazy('articles:article_list')
+    login_url = 'login'
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     fields = ['title', 'body']
     template_name = 'articles/article_new.html'
+    login_url = 'login'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
