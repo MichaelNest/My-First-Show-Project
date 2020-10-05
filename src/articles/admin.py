9fee08afea3f3095ea76b0f243ca101030998706
MichaelNest <nesterenkom385@gmail.com>
2020-10-05 16:02:49 +0300

    Set file articles/admin.py for show comments in adminpanel (admin.StackedInLine and admin.TabularInLine)

--- src/articles/admin.py
+++ src/articles/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
 
-admin.site.register(Article)
+class CommentInLine(admin.StackedInline ): # admin.TabularInLine
+    model = Comment
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [CommentInLine, ]
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
