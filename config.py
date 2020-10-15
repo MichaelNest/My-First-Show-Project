23418c542c5de9e880b1ace22bd3c1af837be0d1
MichaelNest <nesterenkom385@gmail.com>
2020-10-15 18:08:35 +0300

    Move files config.py, .secret.yaml and settings.yaml upstair to src

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ from dynaconf import Dynaconf
 settings = Dynaconf(
     envvar_prefix="MYSITE",
     settings_files=['settings.yaml', '.secrets.yaml'],
+    core_loaders = ['YAML'],
+    yaml_loader = 'safe_load',
 )
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
