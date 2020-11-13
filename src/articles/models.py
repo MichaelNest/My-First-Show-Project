8a4e065e36046945d559944eca3fa29f0bfc810e
MichaelNest <nesterenkom385@gmail.com>
2020-11-13 16:23:20 +0200

    add class Meta to models.py

--- src/articles/models.py
+++ src/articles/models.py
@@ -16,6 +16,9 @@ class Article(models.Model):
     def get_absolute_url(self):
         return reverse('articles:article_detail', args=[str(self.id)])
 
+    class Meta:
+        ordering = ('-create_date',)
+
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
     comment = models.TextField()
